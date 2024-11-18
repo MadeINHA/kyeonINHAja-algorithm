531940e4f36370064741baa7a1b62d193e80af4a
오지환 <ojh3212@naver.com>
2024-11-18 16:13:15 +0900

    feat: Kickboard 클래스 구현 및 헤더 파일 추가
    - kickboard.h : Kickboard 클래스 정의(속성: 'id', 'lat', 'lng', 'cluster_id', 'parking_zone', 'border')
    - kickboard.cpp : Kickboard 데이터를 관리하기 위한 메서드(setters, getters, JSON 변환 메서드 등) 구현
    - DBSCAN 클러스터링 및 Convex hull 알고리즘 관련 기능 지원

---
+++ src/Kickboard.cpp
@@ -0,0 +1,58 @@
+#include "Kickboard.h"
+
+Kickboard::Kickboard(int id, double lat, double lng, int parking_zone)
+        : id(id), lat(lat), lng(lng), cluster_id(-1), parking_zone(parking_zone), border(false) {}
+
+void Kickboard::set_cluster_id(int num) {
+  cluster_id = num;
+}
+
+void Kickboard::set_parking_zone(int num) {
+  parking_zone = num;
+}
+
+int Kickboard::get_id() const {
+  return id;
+}
+
+double Kickboard::get_lat() const {
+  return lat;
+}
+
+double Kickboard::get_lng() const {
+  return lng;
+}
+
+int Kickboard::get_cluster_id() const {
+  return cluster_id;
+}
+
+int Kickboard::get_parking_zone() const {
+  return parking_zone;
+}
+
+std::pair<double, double> Kickboard::get_coordinates() const {
+  return {lat, lng};
+}
+
+std::map<std::string, std::variant<int, double, std::string>> Kickboard::json_return_dbscan() const {
+  return {
+          {"id", id},
+          {"lat", lat},
+          {"lon", lng},
+          {"cluster_id", cluster_id},
+          {"parking_zone", parking_zone},
+          {"border", false}
+  };
+}
+
+std::map<std::string, std::variant<int, double, std::string>> Kickboard::json_return_convex() const {
+  return {
+          {"id", id},
+          {"lat", lat},
+          {"lon", lng},
+          {"cluster_id", cluster_id},
+          {"parking_zone", parking_zone},
+          {"border", border}
+  };
+}
---
+++ src/Kickboard.h
@@ -0,0 +1,36 @@
+#ifndef KICKBOARD_H
+#define KICKBOARD_H
+
+#include <string>
+#include <map>
+#include <utility>
+#include <variant>
+
+class Kickboard {
+private:
+    int id;
+    double lat;
+    double lng;
+    int cluster_id;
+    int parking_zone; // 0 : 주차 금지 구역, 1 : 주차 가능 구역, 2 : 주차 권장 구역
+    bool border; // 가장 바깥에 위치한 킥보드인지 여부
+
+public:
+    Kickboard(int id, double lat, double lng, int parking_zone);
+
+    void set_cluster_id(int num);
+    void set_parking_zone(int num);
+
+    int get_id() const;
+    double get_lat() const;
+    double get_lng() const;
+    int get_cluster_id() const;
+    int get_parking_zone() const;
+
+    std::pair<double, double> get_coordinates() const;
+
+    std::map<std::string, std::variant<int, double, std::string>> json_return_dbscan() const;
+    std::map<std::string, std::variant<int, double, std::string>> json_return_convex() const;
+};
+
+#endif

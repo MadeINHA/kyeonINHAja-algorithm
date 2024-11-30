de98265a3e4bbdeb7b2c850b2bbe5d80535fdb80
오지환 <ojh3212@naver.com>
2024-11-30 14:36:08 +0900

    feat: test_dataset 파일 생성 및 데이터셋 통합, 데이터 생성 함수 추가
    - src/test_dataset.h:
    	- generate_new_data 함수 선언 추가
    
    - src/test_dataset.cpp:
    	- test_dataset_181.cpp 및 test_dataset_550.cpp를 통합
            - generate_new_data 함수 추가:
    		- 기존 데이터셋을 기반으로 랜덤 변동값을 사용해 새로운 데이터를 생성
      		- Kickboard 객체를 활용하여 사용자가 원하는 개수의 데이터를 손쉽게 확장 가능

--- test/test_dataset.h
+++ test/test_dataset.h
@@ -3,9 +3,12 @@
 
 #include "../src/Kickboard.h"
 #include <vector>
+#include <cstdlib>
+#include <ctime>
 
 // 함수 선언
 std::vector<Kickboard> get_test_dataset_181();
 std::vector<Kickboard> get_test_dataset_550();
+std::vector<Kickboard> generate_new_data(const std::vector<Kickboard>& existing_data, int total_count);
 
 #endif // TEST_DATASET_H

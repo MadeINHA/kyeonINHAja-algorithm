b6782bc9e8874f188914ba38d8193e9d359757b9
오지환 <ojh3212@naver.com>
2024-12-03 20:56:15 +0900

    feat: 랜덤 위치 변경 함수 메인 적용
    - main.cpp:
    	- randomly_modify_positions 함수를 활용하여 데이터를 변동시키는 로직 추가
    	- 기존 데이터를 기반으로 50개의 킥보드 위치를 랜덤하게 변형 후 적용

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "src/Kickboard.h"
 #include "src/DBSCAN.h"
 #include "src/ConvexHull.h"
+#include "src/PolygonUtils.h"
 #include "test/test_dataset.h"
 
 #include <iostream>
@@ -10,15 +11,16 @@
 using namespace std;
 
 int main() {
-  srand(static_cast<unsigned>(time(0)));
+  // srand(static_cast<unsigned>(time(0)));
   // 데이터셋 호출
   vector<Kickboard> kickboard_info_list_181 = get_test_dataset_181();
   vector<Kickboard> kickboard_info_list_550 = get_test_dataset_550();
   vector<Kickboard> kickboard_info_list_1000 = generate_new_data(kickboard_info_list_550, 1000);
+  vector<Kickboard> kickboard_info_list_550_random = randomly_modify_positions(kickboard_info_list_550, 50);
 
   // DBSCAN 클러스터링 실행
-  // vector<Kickboard> clustered_kickboards = DBSCAN(kickboard_info_list_550);
-  vector<Kickboard> clustered_kickboards = DBSCAN(kickboard_info_list_1000);
+  vector<Kickboard> clustered_kickboards = DBSCAN(kickboard_info_list_550_random);
+//  vector<Kickboard> clustered_kickboards = DBSCAN(kickboard_info_list_1000);
 
   // DBSCAN JSON 변환
   Json::Value dbscan_json = DBSCANToJson(clustered_kickboards);
@@ -44,6 +46,17 @@ int main() {
   for(int i=1; i<=max_cluster; i++){
     kickboards = clusters[i].kickboard_list;
     border_kickboards = grahamScan(kickboards);
+
+    // kickboard_info_list_550 일 때, ID = 512 킥보드의 Cluster_id = 35 군집 포함 여부 체크를 위한 출력 코드
+//    if(i==35){
+//      Kickboard comp_kick = kickboard_info_list_550[511];
+//
+//      cout << "  Kickboard ID: " << comp_kick.get_id()
+//           << fixed << setprecision(15) << ", Lat: " << comp_kick.get_lat()
+//           << fixed << setprecision(15) << ", Lng: " << comp_kick.get_lng()
+//           << ", PointInPolygon: "<< isPointInPolygon(border_kickboards, comp_kick) << "\n";
+//    }
+
     border_kickboard_list.push_back(border_kickboards);
   }
 

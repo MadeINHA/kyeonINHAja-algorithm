214f617bcdba05e19ad8c1adc4e0b9351a24902a
LEE K.S <kingslee0619@gmail.com>
2024-12-07 14:27:54 +0900

    feat: Json으로 넘어온 킥보드 리스트들을 객체로 변환하는 parser 구현

--- main.cpp
+++ main.cpp
@@ -2,13 +2,47 @@
 #include "src/DBSCAN.h"
 #include "src/ConvexHull.h"
 #include "src/PolygonUtils.h"
-#include "test/test_dataset.h"
+#include "algorithm_service.grpc.pb.h"
+#include "algorithm_service.pb.h"
 
 #include <iostream>
 #include <iomanip>
 #include <vector>
+#include <string>
+#include <grpcpp/grpcpp.h>
 
 using namespace std;
+
+vector<Kickboard> parseKickboards(const std::string& jsonString) {
+    vector<Kickboard> kickboards;
+
+    // JSON 파싱 객체
+    Json::CharReaderBuilder builder;
+    Json::Value root;
+    string errors;
+
+    // JSON 문자열 파싱
+    istringstream stream(jsonString);
+    if (!Json::parseFromStream(builder, stream, &root, &errors)) {
+        throw runtime_error("Failed to parse JSON: " + errors);
+    }
+
+    // kickboard_list 배열 가져오기
+    const Json::Value& kickboardList = root["kickboard_list"];
+    for (const auto& kickboardData : kickboardList) {
+        long id = kickboardData["kickboardId"].asLargestInt();
+        double lat = kickboardData["lat"].asDouble();
+        double lng = kickboardData["lng"].asDouble();
+        int cluster_id = kickboardData["clusterId"].asInt();
+        int parking_zone = kickboardData["parkingZone"].asInt();
+        bool acting = kickboardData["acting"].isNull() ? false : kickboardData["acting"].asBool();
+
+        // Kickboard 객체 생성 및 벡터에 추가
+        kickboards.emplace_back(id, lat, lng, cluster_id, parking_zone, acting);
+    }
+
+    return kickboards;
+}
 
 int main() {
 

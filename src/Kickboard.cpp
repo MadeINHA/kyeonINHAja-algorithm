a79fb4015c50cce80a554de0ee0139e8280168b6
LEE K.S <kingslee0619@gmail.com>
2024-12-07 14:35:20 +0900

    fix: 킥보드 id 타입 변경으로 인한 생성자 수정

--- src/Kickboard.cpp
+++ src/Kickboard.cpp
@@ -1,40 +1,48 @@
 #include "Kickboard.h"
+#include <unordered_map>
 
-Kickboard::Kickboard(int id, double lat, double lng, int parking_zone)
-        : id(id), lat(lat), lng(lng), cluster_id(-1), parking_zone(parking_zone), acting(false) {}
+Kickboard::Kickboard(long id, double lat, double lng, int cluster_id, int parking_zone, bool acting)
+        : id(id), lat(lat), lng(lng), cluster_id(cluster_id), parking_zone(parking_zone), acting(acting) {
+}
+
+Kickboard::Kickboard(long id, double lat, double lng, int parking_zone)
+        :id(id), lat(lat),lng(lng),parking_zone(parking_zone),cluster_id(-1){}
 
 void Kickboard::set_cluster_id(int num) {
-  cluster_id = num;
+    cluster_id = num;
 }
 
 void Kickboard::set_parking_zone(int num) {
-  parking_zone = num;
+    parking_zone = num;
 }
 
-void Kickboard::set_acting(bool state) {
-  acting = state;
-}
-
-int Kickboard::get_id() const {
-  return id;
+long Kickboard::get_id() const {
+    return id;
 }
 
 double Kickboard::get_lat() const {
-  return lat;
+    return lat;
 }
 
 double Kickboard::get_lng() const {
-  return lng;
+    return lng;
 }
 
 int Kickboard::get_cluster_id() const {
-  return cluster_id;
+    return cluster_id;
 }
 
 int Kickboard::get_parking_zone() const {
-  return parking_zone;
+    return parking_zone;
 }
 
 bool Kickboard::get_acting() const {
-  return acting;
+    return acting;
 }
+
+std::pair<double, double> Kickboard::get_coordinates() const {
+    return {lat, lng};
+}
+
+
+
